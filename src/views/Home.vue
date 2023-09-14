c92edbe20799185f297d0817247aceada7330bec
LidorLevi123 <lidorlevi728@gmail.com>
2023-09-14 11:34:11 +0300

    add logo, change background and app title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,6 @@
 <template>
-    <section class="container home text-center">
-        <h1>Home Page</h1>
-        <h5 class="text-center">
-            Powered up Frontend
-        </h5>
-        <img alt="Vue logo" src="../assets/logo.png">
+    <section class="home">
+        <img alt="Vue logo" src="https://skribbl.io/img/logo.gif">
     </section>
 </template>
   
