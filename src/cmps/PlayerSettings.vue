88bcbd2439483b079c89b70f6328a5b5154f020c
LidorLevi123 <lidorlevi728@gmail.com>
2023-09-16 13:44:01 +0300

    change font and style home page

---
+++ src/cmps/PlayerSettings.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="player-settings">
+        <input type="text" class="nickname" placeholder="Enter your name">
+        <select name="" id="">
+            <option value="heb">Hebrew</option>
+        </select>
+
+        <button class="btn play full">Play!</button>
+        <button class="btn create-prv-room full">Create Private Room</button>
+    </section>
+</template>
+  
+<script>
+export default {
+    name: 'player-settings',
+
+    data() {
+        return {
+        }
+    },
+
+    computed: {
+    },
+
+    created() {
+    },
+
+    methods: {
+    }
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,32 @@
 <template>
     <section class="home">
         <img alt="Vue logo" src="https://skribbl.io/img/logo.gif">
+        <PlayerSettings/>
     </section>
 </template>
   
 <script>
+import PlayerSettings from '../cmps/PlayerSettings.vue'
+
 export default {
     name: 'home',
+
     data() {
         return {
         }
     },
+
     computed: {
     },
+
     created() {
     },
+
     methods: {
+    },
+
+    components: {
+        PlayerSettings
     }
 
 }
